b368e10b4207b0bbe78fdc11bd5629368c972824
ryoohshima <ryo.ohshima@actdesign-lab.co.jp>
2020-06-09 10:06:40 +0900

    update

--- js/index.js
+++ js/index.js
@@ -1,11 +1,22 @@
+// メニューボタンのクリック
 $(".header-menu-icon").on("click", function() {
+    // モーダルの表示
     if($(".header-menu-icon-inner div").css("background-color") === "rgb(255, 255, 255)") {
         $(".header-menu-icon-inner div").css("background-color", "rgb(0, 0, 0)");
         $(".header-menu-icon").css("background-color", "rgb(255, 255, 255)");
         $(".header-menu").fadeIn();
+        $(".header-menu-icon-inner div").addClass("open");
+
     } else {
         $(".header-menu-icon-inner div").css("background-color", "rgb(255, 255, 255)");
         $(".header-menu-icon").css("background-color", "rgb(0, 0, 0)");
         $(".header-menu").fadeOut();
+        $(".header-menu-icon-inner div").removeClass("open");
     }
 });
+
+
+
+// .header-menu li:first-child
+// .header-menu li:nth-child(2)
+// .header-menu li:last-child
